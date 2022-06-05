d544894cd4dc4aa03160e8032e6d919421660075
0000Blaze <rchhetry5@gmail.com>
2022-06-05 18:58:32 +0545

    lab 2 number 4

--- lab2/question_4.m
+++ lab2/question_4.m
@@ -1,6 +1,13 @@
 function question_4()
-  c = [1/3 1/2 1/4 1 1/4 1/2 1/3];   %FS coefficients
-  y = 0;
-  N = length(c);
+  t = [-5:0.05:5];
+
+x = 1 + 1/4 * (exp(1i*t) + exp(-1i*t)) + 1/2 * (exp(2i*t) + exp(-2i*t)) + 1/3 * (exp(3i*t) + exp(-3i*t));
+y = 1 + 1/2 * cos(t) + cos(2*t) + 2/3 * cos(3*t);
+
+plot(t,x);
+%plot(t,y,'r');
+xlabel('t');
+ylabel('x(t)');
+title('x(t) = 1 + 1/2cost + cos2t + 2/3cos3t');
   
 endfunction
