f335e2bd8f60ae18dd53a14ecd4340b942ce0b36
0000Blaze <rchhetry5@gmail.com>
2022-06-07 09:11:24 +0545

    removed redundancy

--- lab2/question_2.m
+++ lab2/question_2.m
@@ -47,13 +47,6 @@ hold on;
 plot(t,imag(x),'LineWidth',3,'Color',[0.7,0.7,0.9])
 legend("Real Part","Imaginary Part")
 title('Subplot 1: Taking a as pure imaginary')
-
-subplot(3,2,6)
-C=2;a=0+4j;
-x = C.*exp(a*t);
-plot(t,imag(x),'LineWidth',3,'Color',[0,0.7,0.9])
-xlabel("time");ylabel("Imaginary Part")
-title('Subplot 1: Taking a as pure imaginary')  
 endfunction
 
 function part_c()
